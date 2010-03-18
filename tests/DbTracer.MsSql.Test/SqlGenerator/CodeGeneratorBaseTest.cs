7a6078534f7fa148eab5e365099af14af15c1a27
Petr.Kozelek@gmail.com <petr.kozelek@gmail.com>
2010-03-18 22:48:17 +0100

    Added ViewGenerator tests.

--- tests/DbTracer.MsSql.Test/SqlGenerator/CodeGeneratorBaseTest.cs
+++ tests/DbTracer.MsSql.Test/SqlGenerator/CodeGeneratorBaseTest.cs
@@ -1,8 +1,10 @@
 using DbTracer.Core.Schema.SqlGenerator;
 using DbTracer.MsSql.Model;
+using MbUnit.Framework;
 
 namespace DbTracer.MsSql.Test.SqlGenerator
 {
+    [TestFixture]
     public abstract class CodeGeneratorBaseTest<T> : GeneratorBaseTest<T>
         where T : ICode
     {
---
+++ tests/DbTracer.MsSql.Test/SqlGenerator/ViewGeneratorTest.cs
@@ -0,0 +1,42 @@
+using DbTracer.MsSql.Model;
+using DbTracer.MsSql.SqlGenerator;
+using MbUnit.Framework;
+
+namespace DbTracer.MsSql.Test.SqlGenerator
+{
+    [TestFixture]
+    public class ViewGeneratorTest : CodeGeneratorBaseTest<View>
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        [Test]
+        public void CreateTest()
+        {
+            const string expectedSql = "CREATE VIEW [dbo].[test_view] AS  SELECT * FROM test_table";
+            TestedObject = Mocks.Stub<View>();
+            TestedObject.Definition = expectedSql;
+            var testedGenerator = BuildGenerator(new ViewGenerator(TestedObject));
+            ToCreateSqlTest(testedGenerator, expectedSql);
+        }
+
+        [Test]
+        public void DropTest()
+        {
+            TestedObject = Mocks.Stub<View>();
+            using (Mocks.Record())
+            {
+                TestedObject.Name = "test_view";
+            }
+            using (Mocks.Playback())
+            {
+                const string expectedSql = "DROP VIEW [test_view]";
+                var testedGenerator = BuildGenerator(new ViewGenerator(TestedObject));
+                Utils.AreSqlEqual(expectedSql, testedGenerator.ToDropSql());
+            }
+        }
+    }
+}
